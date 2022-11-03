52619b71bf23243b4a36ea24d24c3dbd13c9e9f6
Toyoabasi Bob <bobtoyoabasi@gmail.com>
2022-11-03 22:41:31 +0100

    style: added pseudo styles to contact file

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -3,11 +3,14 @@ import Footer from '../../Components/Footer/Footer'
 
 const Contact = () => {
 
+  const data ={
+    name: "Toyoabasi Bob"
+  }
 
   return (
     <>
-    <div>
-  <div className="mx-auto max-w-5xl px-4 mt-8 md:mt-40 sm:px-6 lg:px-8 py-5">
+    <div className='md:mb-[5rem]'>
+  <div className="mx-auto max-w-5xl px-4 mt-8 md:mt-[7rem] sm:px-6 lg:px-8 py-5">
 
     <div className='py-7'>
       <h3 className=' text-[36px] font-bold text-[#101828] '>Contact Me</h3>
@@ -15,26 +18,28 @@ const Contact = () => {
     </div>
 
         <form action="" className="space-y-4">
-          
+        
 
           <div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
             <div className='space-y-2'>
               <label className="text-md font-[500] text-[#344054]" for="name">First Name</label>
               <input
-                className="w-full rounded-lg border border-gray-200 p-3 text-md"
+                className="w-full rounded-lg border border-gray-200 p-3 text-md focus:border-[#B2DDFF]"
                 placeholder="Enter your first name"
                 type="text"
                 id="first_name"
+                required
               />
             </div>
 
             <div className='space-y-2'>
               <label className="text-md font-[500] text-[#344054]" for="name">Last Name</label>
               <input
-                className="w-full rounded-lg border border-gray-200 p-3 text-md"
+                className="w-full rounded-lg border border-gray-200 p-3 text-md focus:border-[#B2DDFF]"
                 placeholder="Enter your last name"
                 type="text"
                 id="last_name"
+                required
               />
             </div>
 
@@ -42,20 +47,23 @@ const Contact = () => {
           <div className='space-y-2'>
             <label className="text-md font-[500] text-[#344054]" for="email">Email</label>
             <input
-              className="w-full rounded-lg border border-gray-200 p-3 text-md"
+              className="w-full rounded-lg border border-gray-200 p-3 text-md focus:border-[#B2DDFF]"
               placeholder="yourname@gmail.com"
               type="email"
               id="email"
+              required
             />
           </div>
 
           <div className='space-y-2'>
             <label className="text-md font-[500] text-[#344054]" for="message">Message</label>
             <textarea
-              className="w-full rounded-lg border border-gray-200 p-3 text-md"
+            
+              className="w-full rounded-lg border border-gray-200 p-3 text-md focus:border-[#B2DDFF] "
               placeholder="Send me a message and i'll reply you as soon as possible..."
               rows="8"
               id="message"
+              required
             ></textarea>
           </div>
 
@@ -65,18 +73,20 @@ const Contact = () => {
                 type="checkbox"
                 id="MarketingAccept"
                 name="marketing_accept"
-                className="h-5 w-5 border rounded-[1rem] border-gray-200 bg-white shadow-sm"
+                className="h-5 w-5 border rounded-[1rem] border-gray-200 bg-white shadow-sm "
+                required
               />
 
               <span className="text-md text-[#475467] ">
-               You agree to providing your data to {} who may contact you.
+               You agree to providing your data to {data.name} who may contact you.
               </span>
             </label>
           </div>
 
           <button
         type="submit"
-        className="block w-full rounded-lg bg-[#1570EF] px-5 py-4 text-md font-medium text-white "
+        className="block w-full rounded-lg bg-[#1570EF] hover:bg-[#175CD3] px-5 py-4 text-md font-medium transition text-white "
+        id='btn__submit'
       >
         Send message
       </button>
